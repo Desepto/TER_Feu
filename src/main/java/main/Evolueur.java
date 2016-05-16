9d4a072b260dc291bc93cdf5b3f913ef215cf21d
Desepto <chwebaka@hotmail.fr>
2016-05-16 23:04:56 +0200

    flemme de mettre un message

--- src/main/java/main/Evolueur.java
+++ src/main/java/main/Evolueur.java
@@ -2,7 +2,10 @@ package main;
 
 import java.awt.Point;
 
+import pathfinding.AStar;
+import terrains.Lac;
 import acteurs.Acteur;
+import acteurs.Canadair;
 import acteurs.Feu;
 import entreesSorties.Ecrivain;
 import entreesSorties.Lecteur;
@@ -35,7 +38,7 @@ public class Evolueur {
 	public Evolueur(int tailleCarte, String fichierEntree, String fichierSortie) {
 
 		this.g = new Gestionnaire();
-		this.c = Lecteur.creemapFichier("Carte", g);
+		this.c = Lecteur.creemapFichier("Carte", this.g);
 		this.e = new Ecrivain(fichierSortie);
 		e.initFichier(this.c);
 
@@ -51,18 +54,27 @@ public class Evolueur {
 			for (Acteur a : this.c.getFeu())
 				a.agi(this.c);
 			System.out.println(this.c.getModifications().size());
-			/*
-			 * for (Acteur a : this.c.getPompier()) { if
-			 * (this.c.presenceFeu(a.getX(), a.getY())) a.agi(this.c); else
-			 * a.setActeur(AStar.deplacement(this.c, a), this.c); } for (Acteur
-			 * a : this.c.getCanadair()) { // Si le canadair est plein et sur
-			 * une zone de feu ou s'il est // vide et sur un lac, il agit if
-			 * ((this.c.presenceFeu(a.getX(), a.getY()) && ((Canadair) a)
-			 * .isEstCharge()) || (this.c.getTabHexagones(a.getX(), a.getY())
-			 * instanceof Lac && !((Canadair) a) .isEstCharge())) a.agi(this.c);
-			 * else a.setActeur(AStar.deplacement(this.c, a), this.c); } for
-			 * (Acteur a : this.c.getPluie()) a.agi(this.c);
-			 */
+
+			for (Acteur a : this.c.getPompier()) {
+				if (this.c.presenceFeu(a.getX(), a.getY()))
+					a.agi(this.c);
+				else
+					a.setActeur(AStar.deplacement(this.c, a), this.c);
+			}
+			for (Acteur a : this.c.getCanadair()) {
+				// Si le canadair est plein et sur une zone de feu ou s'il est
+				// vide et sur un lac, il agit
+				if ((this.c.presenceFeu(a.getX(), a.getY()) && ((Canadair) a)
+						.isEstCharge())
+						|| (this.c.getTabHexagones(a.getX(), a.getY()) instanceof Lac && !((Canadair) a)
+								.isEstCharge()))
+					a.agi(this.c);
+				else
+					a.setActeur(AStar.deplacement(this.c, a), this.c);
+			}
+			for (Acteur a : this.c.getPluie())
+				a.agi(this.c);
+
 			try {
 				Evenement e = new Evenement();
 				e = g.getUnEvent(tic);
